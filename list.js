5fc244cad6c9aedea87340f7e8fd186be4d03bb1
Maxence Ramael <maxence.ramael@gmail.com>
2023-08-25 17:41:17 +0200

    Creation formulaire de tache

--- list.js
+++ list.js
@@ -1,13 +1,14 @@
+
+// Fonction de création du formulaire de création de la liste
+
 function ajoutListenerNewList() {
   const btnNewList = document.querySelector(".new-list-btn");
-  btnNewList.addEventListener("click", function() {
-
-   const listSection = document.querySelector("#list-section");
-   const newListForm = document.createElement("h2");
-   newListForm.innerText = "Nouvelle liste";
-
-   listSection.appendChild(newListForm);
-
+  btnNewList.addEventListener("click", function () {
+    const listSection = document.querySelector("#list-section");
+    const newListForm = document.createElement("div");
+    newListForm.innerHTML =
+      "<form><input name='task' placeholder='Nouvelle tache'></input></form>";
+    listSection.appendChild(newListForm);
   });
 }
 
